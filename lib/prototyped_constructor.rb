2da4ae5b48555772ea913254eddf23d9622f8005
Nahuel Andreoli <nahuedu@hotmail.com>
2014-09-11 19:39:51 -0300

    Se agregan los primeros 2 constructores

---
+++ lib/prototyped_constructor.rb
@@ -0,0 +1,37 @@
+require_relative 'metamodel'
+require_relative 'prototyped_object'
+
+class PrototypedConstructor
+
+  attr_accessor :prototype
+  attr_accessor :inicializacion
+
+  def initialize(proto_obj, *args)
+    self.prototype = proto_obj
+    bloque = args[0]
+    if(bloque)
+      self.inicializacion = bloque
+    end
+
+  end
+
+  def new(*args)
+    nuevo_objeto = PrototypedObject.new
+    nuevo_objeto.metamodel = Metamodel.new
+    nuevo_objeto.set_prototype(self.prototype)
+
+    if(self.inicializacion)
+      self.inicializacion.call(nuevo_objeto, *args)
+    else
+      hash = args[0]
+      hash.each_pair {
+          |key, value|
+        nuevo_objeto.send("#{key}=", value)
+      }
+
+    end
+    nuevo_objeto
+  end
+
+
+end
---
+++ spec/prototyped_constructor_spec.rb
@@ -0,0 +1,37 @@
+require 'rspec'
+require_relative '../lib/prototyped_constructor'
+
+describe 'Constructores' do
+
+  let :guerrero do
+    guerrero = PrototypedObject.new
+    guerrero.set_property(:energia, 100)
+    guerrero.set_property(:potencial_defensivo, 10)
+    guerrero.set_property(:potencial_ofensivo, 30)
+    guerrero
+  end
+
+  it 'Parte 1' do
+
+    guerrero_constructor = PrototypedConstructor.new(guerrero, proc {
+        |guerrero_nuevo, una_energia, un_potencial_ofensivo, un_potencial_defensivo|
+      guerrero_nuevo.energia = una_energia
+      guerrero_nuevo.potencial_ofensivo = un_potencial_ofensivo
+      guerrero_nuevo.potencial_defensivo = un_potencial_defensivo
+    })
+    un_guerrero = guerrero_constructor.new(100, 30, 10)
+    expect(un_guerrero.energia).to eq(100)
+
+  end
+
+  it 'Parte 2' do
+
+    guerrero_constructor = PrototypedConstructor.new(guerrero)
+    un_guerrero = guerrero_constructor.new(
+        {energia: 100, potencial_ofensivo: 30, potencial_defensivo: 10}
+    )
+    expect(un_guerrero.potencial_ofensivo).to eq(30)
+
+  end
+
+end
