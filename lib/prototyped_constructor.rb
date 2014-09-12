80c7140d95caceb4d4ba30051f1d914f15817724
Nahuel Andreoli <nahuedu@hotmail.com>
2014-09-12 17:03:53 -0300

    Ya se puede extender constructores

--- lib/prototyped_constructor.rb
+++ lib/prototyped_constructor.rb
@@ -24,6 +24,7 @@ end
 class BaseConstructor
 
   attr_accessor :prototype
+  attr_accessor :super_constructor
 
   def initialize(proto_obj)
     self.prototype = proto_obj
@@ -36,53 +37,83 @@ class BaseConstructor
     nuevo_objeto
   end
 
-  def extended &block
+  def new(*args)
+    nuevo_objeto = self.new_prototyped_object(self.prototype)
+    initialization(nuevo_objeto, *args)
+    nuevo_objeto
+  end
+
+  def initialization(nuevo_objeto, *args)
+
+    if(super_constructor)
+      arguments_needed = self.arguments_needed
+      arguments_total = args.length
+      self_arguments = args.drop(arguments_total - arguments_needed)
+      self.initialize_object(nuevo_objeto, *self_arguments)
+      super_arguments = args.take(arguments_total - arguments_needed)
+      super_constructor.initialize_object(nuevo_objeto, *super_arguments)
+    else
+      self.initialize_object(nuevo_objeto, *args)
+    end
 
   end
 
+  def extended &block
+    new_prototype = self.prototype.clone
+    block.call(new_prototype)
+    new_constructor = BlockConstructor.new(new_prototype, block)
+    new_constructor.super_constructor = self
+    new_constructor
+  end
 end
 
 class CopyConstructor < BaseConstructor
 
-  def new
-    obj = self.prototype
-    new_obj = self.new_prototyped_object(obj)
-    obj.instance_variables.each do
+  def initialize_object(new_obj)
+    self.prototype.instance_variables.each do
     |variable|
-      value = obj.instance_variable_get(variable)
+      value = self.prototype.instance_variable_get(variable)
       new_obj.instance_variable_set(variable, value)
     end
-    new_obj
+  end
+
+  def arguments_needed
+    0
   end
 end
 
 class BlockConstructor < BaseConstructor
 
   attr_accessor :initialization_block
+  attr_accessor :super_constructor
 
   def initialize(proto_obj, block)
     self.prototype = proto_obj
     self.initialization_block = block
   end
 
-  def new(*args)
-    nuevo_objeto = new_prototyped_object(self.prototype)
-    self.initialization_block.call(nuevo_objeto, *args)
-    nuevo_objeto
+  def initialize_object(nuevo_objeto, *args)
+      self.initialization_block.call(nuevo_objeto, *args)
+  end
+
+  def arguments_needed
+    (self.initialization_block.parameters.length) -1
   end
 
 end
 
 class OptionConstructor < BaseConstructor
 
-  def new(*args)
-    nuevo_objeto = new_prototyped_object(self.prototype)
+  def initialize_object(nuevo_objeto, *args)
     hash = args[0]
     hash.each_pair {
         |key, value|
       nuevo_objeto.send("#{key}=", value)
     }
-    nuevo_objeto
+  end
+
+  def arguments_needed
+    1
   end
 
 end
