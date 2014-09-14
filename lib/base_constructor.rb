cda9a48aacaa3c0bc3937fae62dfba174ff8a8db
Nahuel Andreoli <nahuedu@hotmail.com>
2014-09-14 16:15:13 -0300

    Se cambia la forma de extender constructores.

--- lib/base_constructor.rb
+++ lib/base_constructor.rb
@@ -27,11 +27,45 @@ class BaseConstructor
     end
   end
 
+  def new_prototyped_object(prototype)
+    nuevo_objeto = PrototypedObject.new
+    nuevo_objeto.metamodel = Metamodel.new
+    nuevo_objeto.set_prototype(prototype)
+    nuevo_objeto
+  end
+
   def extended &block
-    new_prototype = self.prototype.clone
-    #block.call(new_prototype)
-    new_constructor = BlockConstructor.new(new_prototype, block)
+    #creo el nuevo prototipo
+    new_prototype = new_prototyped_object(self.prototype)
+    block.call(new_prototype)
+
+    #guardo los nuevos atributos en un objeto cualquiera
+    prototipo_descartable = PrototypedObject.new
+    block.call(prototipo_descartable)
+    atributos = prototipo_descartable.instance_variables
+    atributos.delete(:@metamodel)
+
+
+    #creo el nuevo bloque del constructor extendido
+    new_block = Proc.new {
+        |new_obj, *args|
+      array_asoc = atributos.zip(args)
+      array_asoc.each do
+      |element|
+        attribute = element[0]
+        value = element[1]
+        new_obj.instance_variable_set(attribute, value)
+      end
+    }
+    new_constructor = BlockConstructor.new(new_prototype, new_block)
     new_constructor.super_constructor = self
+
+    new_constructor.define_singleton_method(:arguments_needed, Proc.new{
+
+      self.initialization_block.parameters[1].length
+
+    })
+
     new_constructor
   end
 end
