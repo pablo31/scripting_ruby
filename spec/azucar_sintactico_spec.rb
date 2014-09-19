de6b5b3d8e7acc11fc7311a0d1a9066ca6db6882
Eduardo <eduu.mendez@gmail.com>
2014-09-18 23:17:55 -0300

    Merge branch 'master' of C:\Users\Eduardo\RubymineProjects\tadp with conflicts.

--- spec/azucar_sintactico_spec.rb
+++ spec/azucar_sintactico_spec.rb
@@ -45,38 +45,17 @@ describe 'Azucar Sintactico' do
     expect(otro_guerrero.atacar_a(guerrero_proto)).to eq(80)
   end
 
-  it 'Parte 3' do
 
-    guerrero = PrototypedObject.new
-    guerrero_constructor = PrototypedConstructor.new(guerrero) do |una_energia, un_potencial_ofensivo,
+  it 'Parte 3' do
+    guerrero = PrototypedConstructor.new(guerrero) do |una_energia, un_potencial_ofensivo,
         un_potencial_defensivo|
       self.energia = una_energia
       self.potencial_ofensivo = un_potencial_ofensivo
       self.potencial_defensivo = un_potencial_defensivo
     end
 
-    un_guerrero = guerrero_constructor.new(100, 30, 10)
+    un_guerrero = guerrero.new(100, 30, 10)
     expect(un_guerrero.energia).to eq(100)
-  end
-
-  it 'Parte 4' do
-
-    guerrero_constructor = PrototypedConstructor.create {
-      self.atacar_a = proc {|otro_guerrero|
-        if(otro_guerrero.potencial_defensivo < self.potencial_ofensivo)
-          otro_guerrero.recibe_danio(self.potencial_ofensivo - otro_guerrero.potencial_defensivo)
-        end}
-      self.recibe_danio = proc { |danio| self.energia = self.energia - danio}
-    }.with {
-        |una_energia, un_potencial_ofensivo, un_potencial_defensivo|
-      self.energia = una_energia
-      self.potencial_ofensivo = un_potencial_ofensivo
-      self.potencial_defensivo = un_potencial_defensivo
-    }
-
-    guerrero = guerrero_constructor.new(100, 30, 10)
-    expect(guerrero.energia).to eq(100)
-
   end
 
 end
